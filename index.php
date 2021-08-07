88878ddd9cfdb7c106151f4e44732675e54acb48
LefrandSM <lefrandseptiandm@gmail.com>
2021-08-07 09:25:54 +0700

    memisahkan navbar dengan halaman inti,sedang mendesign navbar dropdown

---
+++ index.php
@@ -0,0 +1,243 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- font awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+  <!-- library animasi css -->
+  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+  <!-- my css -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/navbar.css">
+  <link rel="stylesheet" href="css/font-family.css">
+
+  <title>Hello, world!</title>
+</head>
+
+<body>
+
+  <!-- navbar -->
+  <?php include 'navbar.php'; ?>
+  <!-- akhir navbar -->
+
+  <!-- slider -->
+  <div class="slider">
+    <div class="tulisan">
+      <div class="garis">
+        <p data-aos="fade-right" data-aos-duration="500">Our Values</p>
+        <h1 data-aos="fade-right" data-aos-duration="1500">Respect</h1>
+      </div>
+      <p data-aos="zoom-in" data-aos-duration="2000">Respect one other</p>
+    </div>
+    <div class="faded-1">
+      <img src="img/slider-1.jpg" alt="">
+    </div>
+    <div class="faded-2">
+      <img src="img/slider-2.jpg" alt="">
+    </div>
+  </div>
+  <!-- akhir slider -->
+
+  <!-- konten -->
+  <div class="container visi-misi">
+
+    <!-- welcome -->
+
+    <div class="row welcome">
+      <div class="col text-center">
+        <h1 class="judul-welcome">Welcome To Smk Negeri 4 Payakumbuh</h1>
+        <p class="mb-4">ACS Jakarta is an International Baccalaureate World School and an authorized Cambridge Centre offering a world standard education to children from 3 - 18.</p>
+        <span></span>
+      </div>
+    </div>
+
+    <!-- visi misi -->
+
+    <div class="row">
+      <div class="col-lg visi" data-aos="fade-left" data-aos-duration="1000">
+        <h1 class="judul">Visi</h1>
+        <p>Nurturing Leaders for God, Country and Community</p>
+      </div>
+      <div class="col-lg misi" data-aos="fade-left" data-aos-duration="2000">
+        <h1 class="judul">Misi</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo recusandae, maxime, adipisci ut nisi repellendus distinctio non vel accusamus iste sed aut officia assumenda! Ut quisquam error, obcaecati ad deleniti enim quam odio. Obcaecati suscipit excepturi dignissimos voluptatibus quasi. Saepe ipsa est itaque ipsam vitae enim aperiam cupiditate autem, eos tempore modi dicta dolore quasi nam soluta ratione eaque quae quisquam et, accusantium similique eligendi consectetur nisi. Laboriosam cum ipsum vitae expedita dolorum commodi cumque ratione unde veniam autem nam blanditiis, perspiciatis minima nihil, tempore ipsa quas illo mollitia sint alias voluptas sit? Sunt, itaque a? Cupiditate sapiente corrupti voluptatibus.</p>
+        <a href="#" class="lengkap">View detail <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+      </div>
+    </div>
+  </div>
+
+  <div class="konten">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1 class="judul">Terakhir di update</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-6">
+          <h1>Berita & Acara</h1>
+        </div>
+        <div class="col-lg-6">
+          <a href="#" class="berita lengkap">Lihat semua berita & acara <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row" data-aos="fade-left" data-aos-duration="1000">
+        <div class="col-lg-6">
+          <a href="#">
+            <div class="card portal-berita-besar">
+              <div class="card-body">
+                <div class="row header-berita-besar">
+                  <p class="header-berita">News</p>
+                  <p><i class="fa fa-newspaper-o" aria-hidden="true"></i></p>
+                </div>
+                <div class="row">
+                  <h5 class="tanggal-berita">8 AGUSTUS 2021</h5>
+                </div>
+                <div class="row">
+                  <h4>Congratulation to Lefrand septiand maydi</h4>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-3">
+          <a href="">
+            <div class="card berita-kecil">
+              <div class="card-body">
+                <div class="row">
+                  <p class="header-berita">News</p>
+                </div>
+                <div class="row">
+                  <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur, quaerat.</h3>
+                </div>
+                <div class="row garis-bawah-berita">
+                  <span></span>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-3">
+          <a href="#">
+            <div class="card berita-kecil">
+              <div class="card-body">
+                <div class="row">
+                  <p class="header-berita">News</p>
+                </div>
+                <div class="row">
+                  <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur, quaerat.</h3>
+                </div>
+                <div class="row garis-bawah-berita">
+                  <span></span>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row" data-aos="fade-right" data-aos-duration="1500">
+        <div class="col-lg-3">
+          <a href="#">
+            <div class="card berita-kecil">
+              <div class="card-body">
+                <div class="row">
+                  <p class="header-berita">Event</p>
+                </div>
+                <div class="row">
+                  <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur, quaerat.</h3>
+                </div>
+                <div class="row garis-bawah-berita">
+                  <span></span>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-3">
+          <a href="#">
+            <div class="card berita-kecil">
+              <div class="card-body">
+                <div class="row">
+                  <p class="header-berita">Event</p>
+                </div>
+                <div class="row">
+                  <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur, quaerat.</h3>
+                </div>
+                <div class="row garis-bawah-berita">
+                  <span></span>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-6">
+          <a href="#">
+            <div class="card portal-berita-besar">
+              <div class="card-body">
+                <div class="row header-berita-besar">
+                  <p class="header-berita">Event</p>
+                  <p><i class="fa fa-newspaper-o" aria-hidden="true"></i></p>
+                </div>
+                <div class="row">
+                  <h5 class="tanggal-berita">24 Maret 2021</h5>
+                </div>
+                <div class="row">
+                  <h4>Congratulation to Abdullah Khadafi</h4>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="gambar-pembatas">
+    <div class="row">
+      <div class="col">
+        <h1 data-aos="flip-right" data-aos-duration="1200" data-aos-easing="
+          ease-in-out-quart">IT School For Future</h1>
+      </div>
+    </div>
+  </div>
+  <div class="footer-first">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <h4>Smk Negeri 4 Payakumbuh</h4>
+        </div>
+        <div class="col-lg-8">
+          <h5>Lokasi</h5>
+          <p><i class="fa fa-map-marker" aria-hidden="true"></i> Jl. Koto Kociak, Kel.Padang Sikabu, Kec.Lamposi Tigo Nagori, Padang Sikabu, Payakumbuh Lamposi Tigo Nagori, Kota Payakumbuh, Sumatera Barat 26219 </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-second bg-dark text-light text-center py-3">
+    <p data-aos="fade-up" data-aos-duration="1000" data-aos-offset="10">&copy; 2021 Lefrand. All Reversed</p>
+  </div>
+  <!-- akhir konten -->
+
+
+
+  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,74 @@
+<nav class="navbar navbar-expand-md navbar-dark" data-aos="fade-down" data-aos-duration="1000">
+    <a class="navbar-brand" href="#">Smk Negeri 4 Payakumbuh</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+            <a class="nav-link active" href="#">Beranda</a>
+            <!--profile dropdown -->
+            <div class="nav-item dropdown btn-group">
+                <a href="" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Profile
+                </a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <a href="#" class="dropdown-item">Tentang Sekolah</a>
+                    <a href="#" class="dropdown-item">Kepala Sekolah</a>
+                    <a href="#" class="dropdown-item">Sejarah Sekolah</a>
+                    <a href="#" class="dropdown-item">Visi, Misi, dan Filosofi Pendidikan</a>
+                    <!-- dropdown paket keahlian -->
+                    <div class="btn-group dropright">
+                        <a href="#" class="dropdown-item" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Paket Keahlian
+                        </a>
+                        <div class="dropdown-menu">
+                            <a href="#" class="dropdown-item">Rpl</a>
+                            <a href="#" class="dropdown-item">Mm</a>
+                            <a href="#" class="dropdown-item">Tkj</a>
+                        </div>
+                    </div>
+                    <a href="#" class="dropdown-item">Jam Sekolah dan Kalender Tahun Pendidikan</a>
+                    <a href="#" class="dropdown-item">Prestasi Siswa</a>
+                    <a href="#" class="dropdown-item">Lambang dan Lagu Kebangsaan Sekolah</a>
+                </div>
+            </div>
+            <!-- manajemen dropdown -->
+            <div class="nav-item dropdown btn-group">
+                <a href="" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Manajemen
+                </a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <a href="#" class="dropdown-item">Kurikulum</a>
+                    <a href="#" class="dropdown-item">Kesiswaan</a>
+                    <a href="#" class="dropdown-item">Humas</a>
+                    <a href="#" class="dropdown-item">Sarana dan prasarana</a>
+                </div>
+            </div>
+            <!-- info dropdown -->
+            <div class="nav-item dropdown btn-group">
+                <a href="" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Info
+                </a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <a href="#" class="dropdown-item">Agenda</a>
+                    <a href="#" class="dropdown-item">Pengumuman</a>
+                    <a href="#" class="dropdown-item">Acara</a>
+                </div>
+            </div>
+            <!-- publikasi dropdown -->
+            <div class="nav-item dropdown btn-group">
+                <a href="" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Publikasi
+                </a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <a href="#" class="dropdown-item">Karya seni dan literasi</a>
+                    <a href="#" class="dropdown-item">Galeri</a>
+                </div>
+            </div>
+            <!-- kontak -->
+            <a class="nav-link" href="#">Lsp</a>
+            <a class="nav-link" href="#">Alumni</a>
+            <a class="nav-link" href="#">Kontak</a>
+        </div>
+    </div>
+</nav>
